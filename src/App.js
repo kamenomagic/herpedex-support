2e3bd458c8f2fb46c4f1fe2f4cd625fe8f4fcec5
S. Jacob Powell <s.jacob.powell@gmail.com>
2020-10-14 11:52:00 -0600

    Change to herpedex support

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ function App() {
   return (
     <div className="App">
       <h2>
-        This is the support page for the Puzzle Charms/Puzzle Cleots app.
+        This is the support page for the Herpedex app.
       </h2>
       <h2>
-        Feel free to open a new issue with any bugs or problems, please provide screenshots etc. at <a href="https://github.com/kamenomagic/puzzle-charms-support">github.com/kamenomagic/puzzle-charms-support</a>
+        Feel free to open a new issue with any bugs or problems, please provide screenshots etc. at <a href="https://github.com/kamenomagic/herpedex-support">github.com/kamenomagic/herpedex-support</a>
       </h2>
     </div>
   );
